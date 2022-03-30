94d6597273c0da2deb4a2c3872dd7e16db2e56f5
Kirillfed7 <57727960+Kirillfed7@users.noreply.github.com>
2022-03-30 09:28:09 +0300

    Add files via upload

---
+++ BD/BD/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            contactsTableAdapter.Update(testDataSet);
+        }
+
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Удалить запись?", "Удаление", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+            if (dr == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddForm af = new AddForm();
+            af.Owner = this;
+            af.Show();
+        }
+    }
+}
